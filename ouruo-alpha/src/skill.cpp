d62b843ecddc6d10017d52f0f1e6f2a60a232661
Alessandro Sperindé <alessandro.sperinde@gmail.com>
2014-05-14 16:19:50 +0200

    - Pulizia del codice

--- ouruo-alpha/src/skill.cpp
+++ ouruo-alpha/src/skill.cpp
@@ -1,20 +1,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////
 //                                                                                   //
-//	This is OurUO Tech Demo, for more info on this project, please refer to:         //
-//	                                                                                 //
-// 	https://github.com/roguelikes/ouruo-alpha                                        //
-// 		                                                                             //
-// 	Copyright (C) 2013  Developer Riker (https://github.com/dev-riker)               //
+//  This is OurUO Tech Demo, for more info on this project, please refer to:         //
 //                                                                                   //
-// 	This program is free software; you can redistribute it and/or                    //
-// 	modify it under the terms of the GNU General Public License                      //
-// 	as published by the Free Software Foundation; either version 2                   //
-// 	of the License, or (at your option) any later version.                           //
+//  https://github.com/roguelikes/ouruo-alpha                                        //
 //                                                                                   //
-// 	This program is distributed in the hope that it will be useful,                  //
-// 	but WITHOUT ANY WARRANTY; without even the implied warranty of                   //
-// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    //
-// 	GNU General Public License for more details.                                     //
+//  Copyright (C) 2013  Developer Riker (https://github.com/dev-riker)               //
+//                                                                                   //
+//  This program is free software; you can redistribute it and/or                    //
+//  modify it under the terms of the GNU General Public License                      //
+//  as published by the Free Software Foundation; either version 2                   //
+//  of the License, or (at your option) any later version.                           //
+//                                                                                   //
+//  This program is distributed in the hope that it will be useful,                  //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of                   //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    //
+//  GNU General Public License for more details.                                     //
 //                                                                                   //
 // 	You should have received a copy of the GNU General Public License                //
 // 	along with this program; if not, write to the Free Software                      //
@@ -213,8 +213,7 @@ bool SkillTree::AddMinorSkill(Skill *newMinorSkill)
     bool ret = false;
 
     // Controlla che non si sia già inserita una skill minore o maggiore dello stesso tipo nella lista
-    if ((majorSkills_.count(newMinorSkill->GetName()) == 0) && (minorSkills_.count(newMinorSkill->GetName()) == 0))
-    {
+    if ((majorSkills_.count(newMinorSkill->GetName()) == 0) && (minorSkills_.count(newMinorSkill->GetName()) == 0)) {
         minorSkills_[newMinorSkill->GetName()] = newMinorSkill;
         ret = true;
     }
